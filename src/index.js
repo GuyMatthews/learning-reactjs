d3e227f1958907929631f8ae7ce7855d109e2238
Guy Matthews <guymatthews@guys-air.hursley.uk.ibm.com>
2018-11-08 15:38:32 +0000

    API functioning, up to 10.2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import { data } from './data';
-
 import BookList from './components/BookList';
 
 ReactDOM.render(
-  <BookList books={[]} authors={{}} />,
+  <BookList />,
   document.getElementById('root')
 );
