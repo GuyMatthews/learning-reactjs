a5f9750a9becdb532a7bff2ba7f83e19508a5637
Guy Matthews <guymatthews@guys-air.hursley.uk.ibm.com>
2018-11-09 10:50:09 +0000

    Dispatching Actions completed

---
+++ src/reducers.js
@@ -0,0 +1,13 @@
+const mainReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'RECEIVE_BOOKS':
+            return {
+                ...state,
+                books: action.books
+            };
+        default:
+            return state;
+    }
+};
+
+export default mainReducer;
